73f45d84a6741ed79af7f9d2a3eef6c6b8484690
tienhienn <huynhletienhien@gmail.com>
2025-10-18 09:29:21 +0700

    uplan 2

--- BaiKiemTraGiuaKy/app/src/main/java/com/example/baikiemtragiuaky/LoginActivity.java
+++ BaiKiemTraGiuaKy/app/src/main/java/com/example/baikiemtragiuaky/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.baikiemtragiuaky;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+    private String usernameDefault = "tienhien";
+    private String passwodDeflaut = "123";
 
+    EditText edtNhap,edtMatKhau;
+    Button btnDangNhap;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edtNhap = findViewById(R.id.edtNhap);
+        edtMatKhau = findViewById(R.id.edtMatKhau);
+        btnDangNhap = findViewById(R.id.btnDangNhap);
+
     }
 }
